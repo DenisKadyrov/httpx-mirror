3fbd0e37bd0407dd35188c4b7993233135e05df6
Denis <kripmrx@gmail.com>
2025-07-28 17:46:23 +0500

    feat: init config

--- httpx-mirror/main.py
+++ httpx-mirror/main.py
@@ -1,8 +1,12 @@
 from app import App
+from config import Config
+from dotenv import load_dotenv
 from http_client import HttpClient
 from proxy import Proxy
 import uvicorn
 
+load_dotenv()
+config = Config()
 
 client = HttpClient()
 proxy = Proxy(client)
@@ -10,4 +14,4 @@ app = App(proxy)
 proxy_app = app.proxy.app
 
 if __name__ == "__main__":
-    uvicorn.run("main:proxy_app", port=8000, reload=True)
+    uvicorn.run("main:proxy_app", port=8000, reload=config.DEBUG)
