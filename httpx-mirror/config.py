353e5121fa2b9065eec3679c6b312f9f82885976
Denis <kripmrx@gmail.com>
2025-07-28 17:40:48 +0500

    chore(Config): BASE_URL and DEBUG

---
+++ httpx-mirror/config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+
+class Config:
+    """
+    Application configurations
+
+    """
+    def __init__(self):
+        self.BASE_URL = os.getenv('BASE_URL')
+
+        if not self.BASE_URL:
+            raise ValueError("Missing required environment variable: BASE_URL")
+
+        self.DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
+
